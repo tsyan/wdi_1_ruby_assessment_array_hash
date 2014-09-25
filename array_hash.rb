52705e7802f75f58e3ea8fe67209dfefb7087153
cyurtbil <cenky92@gmail.com>
2014-09-25 09:15:35 -0400

    arrays and hashes quiz  done

--- array_hash.rb
+++ array_hash.rb
@@ -8,20 +8,28 @@
 
 pets = ['cat', 'dog']
 
+pets << 'snake'
+
 # Question 2
 # Write code to return the value of the key :age in the friend hash
 friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
+
+friend[:age]
 
 # Question 3
 # Write code to make all the elements in the pet array be uppercase.
 # The result should be a new array, and the old pet array should still exist.
 
+new_pets = pets.map do |pet|
+	pet.upcase
+end	
+
 # Question 4
 # Write code to retrieve the index of 'dog' in the original pet array.
-
+pets.index('dog')
 # Question 5
 # Write code to change the value of :favorite_food in the friend hash to 'sushi'.
-
+friend[:favorite_food] = "sushi"
 # Question 6
 # Look at the following code. Explain in your own words why the argument
 # on first line (r) can be called something different from the argument on the last line (lemur)
@@ -33,3 +41,7 @@ end
 
 lemur = 8
 puts circumference(lemur)
+
+# answer for Q6 -->> the name of the arguments (r in this case) can be changed to any name if you change the arguments passing to the code in the method
+# if we are going to change the argument r, we should change the r in the 2*pi*r too. It will not affect the lemur because we are passing to the value 
+# to the method not assigning it when we are calling it, so it will be ok to change the argument names.
