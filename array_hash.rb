5bfb094a4dea59a344de26195f35fb8bf1d9124f
MDROR <dymmchl@yahoo.com>
2014-09-25 09:16:20 -0400

    -m "commit quiz"

--- array_hash.rb
+++ array_hash.rb
@@ -6,26 +6,30 @@
 # On a new line, write code to insert the value `snake` at the end of the pets array.
 # Don't modify the original array creation line.
 
-pets = ['cat', 'dog']
+pets = ['cat', 'dog']<<'snake'
 
 # Question 2
 # Write code to return the value of the key :age in the friend hash
 friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
 
+friend[:age]
+
 # Question 3
 # Write code to make all the elements in the pet array be uppercase.
 # The result should be a new array, and the old pet array should still exist.
-
+pets.each do |p|
+  p.upcase
+end
 # Question 4
 # Write code to retrieve the index of 'dog' in the original pet array.
-
+pet('dog')
 # Question 5
 # Write code to change the value of :favorite_food in the friend hash to 'sushi'.
-
+friend(favorite_food = 'sushi')
 # Question 6
 # Look at the following code. Explain in your own words why the argument
 # on first line (r) can be called something different from the argument on the last line (lemur)
-# but the code still works. 
+# but the code still works.
 
 def circumference(r)
   2 * pi * r
@@ -33,3 +37,7 @@ end
 
 lemur = 8
 puts circumference(lemur)
+
+r is just an argument or 'placeholder' for a value to calculate circumference
+lemur which = 8 is the input and so lemur which is 8 gets inputed into the method for
+for circumference which returns circumference
