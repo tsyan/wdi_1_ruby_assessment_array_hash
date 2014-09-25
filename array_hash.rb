cfcc127b79e5f87b8dd8ab92a058ce9f180021d5
CollinBlinder <collinblinder@gmail.com>
2014-09-25 09:27:26 -0400

    Here it is

--- array_hash.rb
+++ array_hash.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Try to not use any reference for this one. If you need to use a reference,
 # please note such in a comment below your answer.
 # You'll still recieve credit, but it's useful for me to know.
@@ -7,25 +8,36 @@
 # Don't modify the original array creation line.
 
 pets = ['cat', 'dog']
+pets << 'snake'
 
 # Question 2
 # Write code to return the value of the key :age in the friend hash
 friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
+age = friend[:age]
+puts age #return does not work for me:: I get a LocalJumpError when I invoke return at this line:: puts works
+
 
 # Question 3
 # Write code to make all the elements in the pet array be uppercase.
 # The result should be a new array, and the old pet array should still exist.
+pets_upcase = []
+ pets.map {|i| pets_upcase << i.upcase}
+
 
 # Question 4
 # Write code to retrieve the index of 'dog' in the original pet array.
+pets.index('dog')
+
 
 # Question 5
 # Write code to change the value of :favorite_food in the friend hash to 'sushi'.
+friend[favorite_food: ="sushi" #I did not complete this line
+binding.pry
 
 # Question 6
 # Look at the following code. Explain in your own words why the argument
 # on first line (r) can be called something different from the argument on the last line (lemur)
-# but the code still works. 
+# but the code still works.
 
 def circumference(r)
   2 * pi * r
@@ -33,3 +45,5 @@ end
 
 lemur = 8
 puts circumference(lemur)
+#This is because 'r' is the variable of the method circumference; lemur is the argument passed when that method is called.
+
