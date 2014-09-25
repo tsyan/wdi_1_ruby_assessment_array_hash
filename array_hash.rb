829412ed42e317be018de4054c179b186fdc3981
Lauren Fazah <laurenfazah@gmail.com>
2014-09-25 09:17:45 -0400

    Finished assessment.

--- array_hash.rb
+++ array_hash.rb
@@ -8,13 +8,24 @@
 
 pets = ['cat', 'dog']
 
+pets << 'snake'
+
+puts pets
+
 # Question 2
 # Write code to return the value of the key :age in the friend hash
 friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
 
+puts friend[:age]
+
 # Question 3
 # Write code to make all the elements in the pet array be uppercase.
 # The result should be a new array, and the old pet array should still exist.
+
+pets.map do |pet|
+  puts pet.upcase
+end
+
 
 # Question 4
 # Write code to retrieve the index of 'dog' in the original pet array.
@@ -22,14 +33,19 @@ friend = { name: "Chris", favorite_food: "Pizza", age: 27 }
 # Question 5
 # Write code to change the value of :favorite_food in the friend hash to 'sushi'.
 
+friend[:favorite_food] = 'sushi'
+puts friend
+
 # Question 6
 # Look at the following code. Explain in your own words why the argument
 # on first line (r) can be called something different from the argument on the last line (lemur)
-# but the code still works. 
+# but the code still works.
 
-def circumference(r)
-  2 * pi * r
-end
+# def circumference(r)
+#   2 * pi * r
+# end
 
-lemur = 8
-puts circumference(lemur)
+# lemur = 8
+# puts circumference(lemur)
+
+# This code can work because as long as the argument passed into a method as it is being defined is the same as it would be used in the computatation, that argument can be called anything. Methods are designed to be used over again, so  multiple different things will be passed into it. This particular code above will not work (hence why I've commented it out)because 'pi' was not defined as a variable (there of course is the option to use the built in Math.pi), but in theory, lemur, a defined variable in this code, will work in the circumference method.
